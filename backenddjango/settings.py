c7c58f27546af602c155b5b0e1669c3d03c5593e
Anuj Krishna <anuj.krishna.phuyal@gmail.com>
2024-05-14 21:59:15 +0545

    some changes in settings

--- backenddjango/settings.py
+++ backenddjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-17pli%zu3ou9h+px2co1zw00^v1nmjjg_%)w%nx=7qhje-@p=e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -126,12 +126,13 @@ USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://192.168.56.1:3000'
+    'http://192.168.56.1:3000',
+    "https://rentalforyou.netlify.app/",
 ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
